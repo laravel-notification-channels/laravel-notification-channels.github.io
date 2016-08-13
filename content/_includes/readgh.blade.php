a656d4ef0235ca9182cfca20a9dfc6d52167b19b
Mohamed Said <themohamedsaid@gmail.com>
2016-08-13 19:04:04 +0200

    publishing website

--- content/_includes/readgh.blade.php
+++ content/_includes/readgh.blade.php
@@ -1,11 +1,29 @@
+<style>
+    .realContent p:first-of-type{
+        display: none;
+    }
+
+    .realContent h2:first-of-type{
+        display: none;
+    }
+
+    .realContent ul:first-of-type{
+        display: none;
+    }
+</style>
+
 <script>
     var file = '{{$repoName}}',
             baseURL = 'https://raw.githubusercontent.com/laravel-notification-channels/' + file + '/master/README.md';
 
-    $.get(baseURL, function (data, status) {
+    $.get(baseURL, function (data) {
         var md = window.markdownit();
         var result = md.render(data);
 
         $('.realContent').prepend(result);
     });
 </script>
+
+<hr>
+
+<a class="btn btn-default" href="https://github.com/laravel-notification-channels/{{$repoName}}/edit/master/README.md">Edit on GitHub</a>
